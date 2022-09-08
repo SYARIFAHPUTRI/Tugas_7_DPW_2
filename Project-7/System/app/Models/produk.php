fdb38b8c9260de5026b8f4f12666d79f84a90f84
SYARIFAHPUTRI <syarifahktp211@gmail.com>
2022-09-08 17:03:38 +0700

    Tugas 7 DPW 2

---
+++ Project-7/System/app/Models/produk.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+// use Illuminate\Database\Eloquent\Model;
+
+class Produk extends Model {
+    protected $table = 'produk';
+
+    function seller(){
+        return $this->belongsTo(User::class, 'id_user');
+    }
+}
